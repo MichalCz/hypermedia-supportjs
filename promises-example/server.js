be93ca31099f9de1cf04531cfb480af063947081
Budleigh Salterton <cz@signicode.com>
2017-04-19 12:26:45 +0200

    minor changes

--- promises-example/server.js
+++ promises-example/server.js
@@ -1,17 +1,24 @@
 const http = require("http-server");
 const request = require('request-promise');
 
+const urllist = ['http://localhost:28211/test1.json', 'http://localhost:28211/test2.json'/*, 'http://localhost:28211/test23.json' */];
+
 new Promise((res, rej) =>
     http.createServer({
             root: `${__dirname}/public`
         }).listen(
             28211, (err) => err ? rej(err) : res()
         )
-).then(
-    () => Promise.all([
-        request('http://localhost:28211/test1.json'),
-        request('http://localhost:28211/test2.json')
-    ])
+)
+.then(
+    () => Promise.all(
+        urllist.map(
+            (url) => request(url)
+        )
+    )
+)
+.then(
+    (arr) => arr.map(JSON.parse)
 )
 .then(
     console.log
@@ -20,7 +27,7 @@ new Promise((res, rej) =>
     () => console.log("done")
 )
 .catch(
-    (e) => console.log("Error occurred", e + '')
+    (e) => console.log("Error occurred", e && e.stack)
 )
 .then(
     () => process.exit(0)
